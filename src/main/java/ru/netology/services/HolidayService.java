d886ec02997c20fc4a16291a27e59143eea70996
Veronika Obukhova <vera.obukhova.95@gmail.com>
2025-03-30 22:47:57 +0700

    Change HolidayService

--- src/main/java/ru/netology/services/HolidayService.java
+++ src/main/java/ru/netology/services/HolidayService.java
@@ -4,7 +4,7 @@ public class HolidayService {
     public int calculate(int income, int expenses, int threshold) {
         int count = 0;
         int money = 0;
-        for (int month = 1; month <= 12; month++) {
+        for (int month = 0; month < 12; month++) {
             if (money >= threshold) {
                 count++;
                 money = (money - expenses) / 3;
